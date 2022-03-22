a39bfb18d133ef1598498abcaabcad783f3fe1ca
Yoshimichi ANDOH <yandoh@chembio.nagoya-u.ac.jp>
2022-03-22 21:26:58 +0900

    programs simplified.

--- VISCOSITY1/2_integrage_acf.f90
+++ VISCOSITY1/2_integrage_acf.f90
@@ -3,7 +3,6 @@
 !https://opensource.org/licenses/mit-license.php
 
         real(8), parameter :: dt=2e-15
-!       integer(4), parameter :: imax=10000000
 !       integer(4), parameter :: imax=5000000  ! 10ns
         integer(4), parameter :: imax=2500000  ! 5ns
 !       integer(4), parameter :: imax=1000000   ! 2ns
@@ -19,14 +18,12 @@
         real(8),allocatable :: Ct(:,:)
         integer(4),allocatable :: nhit(:)
         real(8) :: sekibun(6)
-        real(8) :: sekibun_diag
         real(8) :: sekibun_nond
         real(8) :: aved,avend
         real(8) :: maxaved,minaved
         real(8) :: maxavend,minavend
 
         sekibun=0
-        sekibun_diag=0
         sekibun_nond=0
 
 !#  5.499431780322072E-26  1.358765895952437E-05   2500000
@@ -34,52 +31,31 @@
         allocate(Ct(6,ndat),nhit(ndat))
         do i=1,ndat
           read(*,*) j, Ct(1:6,i), nhit(i)
+         !sekibun(:)=sekibun(:)+dt*Ct(:,i)
+         !sekibun_nond=sekibun_nond+dt*(Ct(4,i)+Ct(5,i)+Ct(6,i))
           if(i==1 .or. i==imax)then
             sekibun(:)=sekibun(:)+0.5d0*dt*Ct(:,i)
-            sekibun_diag=sekibun_diag+0.5d0*dt*(Ct(1,i)+Ct(2,i)+Ct(3,i))
             sekibun_nond=sekibun_nond+0.5d0*dt*(Ct(4,i)+Ct(5,i)+Ct(6,i))
           else
             sekibun(:)=sekibun(:)+1.0d0*dt*Ct(:,i)
-            sekibun_diag=sekibun_diag+1.0d0*dt*(Ct(1,i)+Ct(2,i)+Ct(3,i))
             sekibun_nond=sekibun_nond+1.0d0*dt*(Ct(4,i)+Ct(5,i)+Ct(6,i))
           endif
           write(*,'(f12.3,8es23.15)') j*dt*1e+12, &
-      &        sekibun_diag/3d0,sekibun_nond/3d0,sekibun(1:6)
+      &        sekibun_nond/3d0,sekibun(4:6)
           if(i==imax) exit
         enddo
 
-        sekibun_diag=sekibun_diag/3d0
         sekibun_nond=sekibun_nond/3d0
 
-        aved =(sekibun(1)+sekibun(2)+sekibun(3))/3d0
-        maxaved=maxval(sekibun(1:3))
-        minaved=minval(sekibun(1:3))
         avend=(sekibun(4)+sekibun(5)+sekibun(6))/3d0
         maxavend=maxval(sekibun(4:6))
         minavend=minval(sekibun(4:6))
         write(*,4)'#imax=',imax
-        write(*,3)'#>>  Viscosity(bulk)  / Pa*sec  <<<'
-!       write(*,1) sekibun_diag
-        write(*,2)'#=====  Ave.  =========', &
-                  '#=====  Max.  =========', &
-                  '#=====  Min.  =========|'
-        write(*,1)'#', aved, maxaved, minaved
         write(*,3)'#>>  Viscosity(shear) / Pa*sec  <<<'
-!       write(*,1) sekibun_nond
         write(*,2)'#=====  Ave.  =========', &
                   '#=====  Max.  =========', &
                   '#=====  Min.  =========|'
         write(*,1)'#',avend, maxavend, minavend
-!       write(*,*) 
-!       write(*,3)'#>>  Integrated value / Pa*sec  <<<'
-!       write(*,2)'#=====   xx   =========', &
-!                 '#=====   yy   =========', &
-!                 '#=====   zz   =========|'
-!       write(*,1)'#', sekibun(1:3)
-!       write(*,2)'#=====   xy   =========', &
-!                 '#=====   xz   =========', &
-!                 '#=====   yz   =========|'
-!       write(*,1)'#', sekibun(4:6)
 
 1       format(a,3es23.15)
 2       format(3a)
