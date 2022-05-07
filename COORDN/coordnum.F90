18d1ff77f115e8ca7749aa71e75ea81526e91836
Yoshimichi ANDOH <yandoh@chembio.nagoya-u.ac.jp>
2022-05-07 18:21:41 +0900

    modification for multicomponent system.

--- COORDN/coordnum.F90
+++ COORDN/coordnum.F90
@@ -2,7 +2,6 @@
 !Released under the MIT license
 !https://opensource.org/licenses/mit-license.php
 
-! Stand alone version of molgr.F90
 !
 ! === inputs ===
 ! (1) ./result.gr
@@ -10,9 +9,10 @@
 	implicit none
 	character(1) cdum
 	real(8),allocatable::r(:),gr(:,:)
+	real(8),allocatable::rho(:)
 	integer(4) i
         integer(4) nparts, ndr, nflame
-        real(8) volume, rho, dr, pi
+        real(8) volume, dr, pi
         real(8) rr,dv
         real(8),allocatable::dn(:),sn(:)
 
@@ -20,7 +20,8 @@
 
 !### read g(r) ###
 	read(*,*) cdum, volume, nparts, ndr, nflame
-	read(*,*) cdum, rho
+        allocate( rho(nparts) )
+	read(*,*) cdum, rho(1:nparts)
 	allocate(r(ndr),gr(ndr,nparts))
 	allocate(dn(nparts),sn(nparts))
 	do i=1,ndr
@@ -33,7 +34,7 @@
 	do i=1,ndr
           rr=r(i)
           dv=4d0*pi*(rr**2)*dr
-          dn(1:nparts)=rho*dv*gr(i,1:nparts)
+          dn(1:nparts)=rho(1:nparts)*dv*gr(i,1:nparts)
           sn(1:nparts)=sn(1:nparts)+dn(1:nparts)
           write(*,'(f15.3,99f15.7)') rr, &
      &             gr(i,1:nparts), sn(1:nparts)
