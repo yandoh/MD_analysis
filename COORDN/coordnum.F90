1f5bac1f402e9b4e5586b82c0bc7eb384bf42689
Yoshimichi ANDOH <yandoh@chembio.nagoya-u.ac.jp>
2022-05-15 15:38:24 +0900

    KB parameter added.

--- COORDN/coordnum.F90
+++ COORDN/coordnum.F90
@@ -15,6 +15,7 @@
         real(8) volume, dr, pi
         real(8) rr,dv
         real(8),allocatable::dn(:),sn(:)
+        real(8),allocatable::dKB(:),sKB(:)
 
 	pi=acos(-1d0)
 
@@ -24,6 +25,7 @@
 	read(*,*) cdum, rho(1:nparts)
 	allocate(r(ndr),gr(ndr,nparts))
 	allocate(dn(nparts),sn(nparts))
+	allocate(dKB(nparts),sKB(nparts))
 	do i=1,ndr
           read(*,*) r(i),gr(i,1:nparts)
 	enddo
@@ -31,13 +33,16 @@
 
 !### calc. coordinate number ###
         sn=0d0
+        sKB=0d0
 	do i=1,ndr
           rr=r(i)
           dv=4d0*pi*(rr**2)*dr
           dn(1:nparts)=rho(1:nparts)*dv*gr(i,1:nparts)
           sn(1:nparts)=sn(1:nparts)+dn(1:nparts)
+          dKB(1:nparts)=(gr(i,1:nparts)-1d0)*dv
+          sKB(1:nparts)=sKB(1:nparts)+dKB(1:nparts)
           write(*,'(f15.3,99f15.7)') rr, &
-     &             gr(i,1:nparts), sn(1:nparts)
+     &             gr(i,1:nparts), sn(1:nparts), sKB(1:nparts)
 	enddo
 
 	stop
