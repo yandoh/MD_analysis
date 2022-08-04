10d63dafdd9c35d8b67e4487713e630ffe09af13
Yoshimichi ANDOH <yandoh@chembio.nagoya-u.ac.jp>
2022-08-04 18:51:16 +0900

    readme updated.

--- ATOMGR/water/go.sh
+++ ATOMGR/water/go.sh
@@ -1,4 +1,4 @@
-ifort -O3 ../atomgr_sa.F90 -o ../atomgr
-#gfortran -O3 ../atomgr_sa.F90 -o ../atomgr
+COMPILER=gfortran
+$COMPILER -O3 ../atomgr_sa.F90 -o ../atomgr
 ln -sf water_npt_10ps.dcd ./DCD
 ../atomgr > result.gr
