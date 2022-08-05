ea14ba00f8adc8e4ad3854dda33fef96a0805695
Yoshimichi ANDOH <yandoh@chembio.nagoya-u.ac.jp>
2022-08-05 22:43:40 +0900

    go.sh updated.

--- SelfD/water/go.sh
+++ SelfD/water/go.sh
@@ -1,4 +1,4 @@
-#ifort -O3 ../SelfD_sa.F90 -o ../SelfD
-gfortran -O3 ../SelfD_sa.F90 -o ../SelfD
+COMPILER=gfortran
+$COMPILER -O3 ../SelfD_sa.F90 -o ../SelfD
 ln -sf water_npt_10ps.dcd ./DCD
 ../SelfD > result.selfd
